31672694a3e6afbbfcd47e38557e6bffb5e92f81
Vitali Skarbinski <sfc-ruper@mail.ru>
2022-05-22 19:50:16 +0300

    feat: 04-copy-directory finished

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
 
+const path = require('path')
+const fs = require('fs')
+const pathJoin = path.join(__dirname, 'files')
+const pathCopy = path.join(__dirname, 'files-copy')
+
+const copyFolder = () => {
+   fs.mkdir(pathCopy, { recursive: true }, error => {
+      if (error) throw error
+   })
+
+   fs.readdir(pathCopy, { withFileTypes: true }, (error, files) => {
+      if (error) { console.log(error) }
+      else {
+         files.forEach(file => {
+            fs.unlink(path.join(pathCopy, file.name), error => {
+               if (error) throw error
+            })
+         })
+      }
+   })
+
+   fs.readdir(pathJoin, { withFileTypes: true }, (error, files) => {
+      if (error) {
+         console.log(error)
+      } else {
+         files.forEach(file => {
+            if (file.isFile()) {
+               fs.copyFile(path.join(pathJoin, file.name), path.join(pathCopy, file.name), error => {
+                  if (error) throw error;
+               })
+            }
+         })
+      }
+   })
+}
+copyFolder()
